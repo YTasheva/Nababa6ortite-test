4bc5328a305cc16f9df9020de95930a96e117092
Hiena <stellatasheva@gmail.com>
2023-12-04 00:48:31 +0000

    Added the questions and answers.

---
+++ starter/assets/js/questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+      question: "What should be the very last thing in an HTML document?",
+      answers: ["The heading", "Title", "Body", "Doc type"],
+      correct: 4,
+    },
+    {
+      question: "Which of the following is described as a collection of images put in a single image?",
+      answers: ["Float", "Align", "Sprite", "Image"],
+      correct: 3,
+    },
+    {
+      question: "A collection of data containing both properties and methods is called...",
+      answers: ["Tag", "Selector", "Object", "Class"],
+      correct: 3,
+    },
+    {
+      question: "In JavaScript, 'this' refers to the object that ____ the object.",
+      answers: ["Receives", "Depends", "Owns", "Direct"],
+      correct: 3,
+    },
+    {
+      question: "How can you clear a floated element?",
+      answers: ["Clear:both", "Press the delete key", "Del tag", "Strike tag"],
+      correct: 1,
+    },
+  ];
