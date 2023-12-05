e6e59e49ac3452996e879e130441b22f504a8702
Hiena <stellatasheva@gmail.com>
2023-12-05 12:53:38 +0000

    Updated the questions answers.

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,52 +1,27 @@
 var questions = [
     {
       question: "What should be the very last thing in an HTML document?",
-      answers: {
-       a: "The heading", 
-       b: "Title", 
-       c: "Body", 
-       d: "Doc type",
-      },
-      correct: "d"
+      answer: ["a. Doctype", "b. Title", "c. Body", "d. The heading"],
+      correct: "a. Doctype"
     },
     {
       question: "Which of the following is described as a collection of images put in a single image?",
-      answers: {
-        a: "Float", 
-        b: "Sprite", 
-        c: "Align", 
-        d: "Image",
-      },
-      correct: "c"
+      answers: ["a. Float", "b. Align", "c. Sprite", "d. Image"],
+      correct: "c. Sprite"
     },
     {
       question: "A collection of data containing both properties and methods is called...",
-      answers: {
-        a: "Tag",
-        b: "Selector", 
-        c: "Class", 
-        d: "Object",
-      },
-      correct: "d"
+      answers: ["a. Object", "b. Selector", "c. Class", "d. Tag"],
+      correct: "a. Object"
     },
     {
       question: "In JavaScript, 'this' refers to the object that ____ the object.",
-      answers: {
-        a: "Owns", 
-        b: "Receives", 
-        c: "Depends", 
-        d: "Direct",
-      },
-      correct: "a"
+      answers: ["a. Direct", "b. Receives", "c. Depends", "d. Owns"],
+      correct: "d. Owns"
     },
     {
       question: "How can you clear a floated element?",
-      answers: {
-        a:"Del tag", 
-        b: "Press the delete key", 
-        c: "Strike tag", 
-        d: "Clear:both",
-      },
-      correct: "d"
-    }
+      answers: ["a. Del tag", "b. Press the delete key", "c. Clear:both", "d. Strike tag"],
+      correct: "c. Clear:both"
+    },
   ];
