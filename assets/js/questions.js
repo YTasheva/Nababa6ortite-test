ec0a6d12fab8b931fbad6c67814ed24e2125e82b
Hiena <stellatasheva@gmail.com>
2023-12-05 15:32:47 +0000

    Fixed a typo in questions.js.

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,27 +1,27 @@
 var questions = [
     {
       question: "What should be the very last thing in an HTML document?",
-      answer: ["Doctype", "Title", "Body", "The heading"],
-      correct: 0
+      options: ["Doctype", "Title", "Body", "The heading"],
+      answer: 0
     },
     {
       question: "Which of the following is described as a collection of images put in a single image?",
-      answers: ["Float", "Align", "Sprite", "Image"],
-      correct: 2
+      options: ["Float", "Align", "Sprite", "Image"],
+      answer: 2
     },
     {
       question: "A collection of data containing both properties and methods is called...",
-      answers: ["Object", "Selector", "Class", "Tag"],
-      correct: 0
+      options: ["Object", "Selector", "Class", "Tag"],
+      answer: 0
     },
     {
       question: "In JavaScript, 'this' refers to the object that ____ the object.",
-      answers: ["Direct", "Receives", "Depends", "Owns"],
-      correct: 3
+      options: ["Direct", "Receives", "Depends", "Owns"],
+      answer: 3
     },
     {
       question: "How can you clear a floated element?",
-      answers: ["Del tag", "Press the delete key", "Clear:both", "Strike tag"],
-      correct: 2
+      options: ["Del tag", "Press the delete key", "Clear:both", "Strike tag"],
+      answer: 2
     },
   ];
