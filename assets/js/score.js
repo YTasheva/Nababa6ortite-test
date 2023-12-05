920efbfa79d17aea2c3a8e8be5ce021d95ba76fb
Hiena <stellatasheva@gmail.com>
2023-12-05 13:28:47 +0000

    Added score.js folder.

---
+++ assets/js/score.js
@@ -0,0 +1,10 @@
+var hScores = document.querySelector(".showScores");
+let retString = localStorage.getItem("dataKey");
+let retArray = JSON.parse(retString);
+console.log(retArray);
+
+hScores.innerHTML = (retArray[0]["initials"] + " " + retArray[0]["score"]); 
+
+hScores.innerHTML = (retArray[1]["initials"] + " " + retArray[1]["score"]); 
+hScores.innerHTML = (retArray[2]["initials"] + " " + retArray[2]["score"]); 
+hScores.innerHTML = (retArray[3]["initials"] + " " + retArray[3]["score"]); 
