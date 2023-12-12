f6b0b19848b0030ac1ed7d3d4dc918c164a9948f
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2023-12-12 15:59:05 +0000

    Updated the comments of score.js

--- assets/js/score.js
+++ assets/js/score.js
@@ -1,21 +1,21 @@
-// Step 1: Retrieve the high scores from local storage
+// Retrieve the high scores from local storage
 var highscores = localStorage.getItem("scores");
 
-// Step 2: Parse the JSON string into a JavaScript array
+// Parse the JSON string into a JavaScript array
 var scoresArray = JSON.parse(scores);
 
-// Step 3: Select the HTML element where you want to display the high scores
+// Select the HTML element where you want to display the high scores
 var highScore = document.getElementById("scores");
 
-// Step 4: Loop over the array of high scores
+// Loop over the array of high scores
 for (var i = 0; i < scoresArray.length; i++) {
-  // Step 5: For each high score, create a new HTML element
+  // For each high score, create a new HTML element
   var newScoreElement = document.createElement("li");
 
-  // Step 6: Set the text content of the new element to the high score
+  // Set the text content of the new element to the high score
   newScoreElement.textContent = scoresArray[i].initials + ": " + scoresArray[i].score;
 
-  // Step 7: Append the new element to the selected HTML element
+  // Append the new element to the selected HTML element
   highScoresList.appendChild(newScoreElement);
 }
 
