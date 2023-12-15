5301edb989c77c18a3e40cd24d6c95236c0aeacf
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2023-12-15 17:12:31 +0000

    Fix high scores display and clear button functionality

--- assets/js/score.js
+++ assets/js/score.js
@@ -2,25 +2,25 @@
 var highScoresList = document.querySelector("highscores");
 
 // Parse the JSON string into a JavaScript array
-var scoresArray = highscores ? JSON.parse(highscores) : [];
+var scoresArray = JSON.parse(localStorage.getItem("score")) || [];
 
 // Select the HTML element to display the high scores
-var highScoresList = document.getElementById("scores");
+var scoresListElement = document.querySelector("score");
 
 // Loop over the array of high scores
 for (var i = 0; i < scoresArray.length; i++) {
   var newScoreElement = document.createElement("li");
   newScoreElement.textContent = scoresArray[i].initials + ": " + scoresArray[i].score;
-  highScoresList.appendChild(newScoreElement);
+  scoresListElement.appendChild(newScoreElement);
 }
 
 // Remove the high scores from local storage
 var clearBtn = document.getElementById("clear");
 clearBtn.addEventListener("click", function () {
-  localStorage.removeItem("scores");
-  highScoresList.innerHTML = "";
+  localStorage.removeItem("score");
+  scoresListElement.innerHTML = "";
 });
 
-// localStorage.clear();
+
 
 
