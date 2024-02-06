41dd12123263e854f2e7e22109777d7278a5dd81
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-02-06 17:18:49 +0000

    Update question options and answers

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,27 +1,27 @@
 var questions = [
     {
       question: "What should be the very last thing in an HTML document?",
-      options: ["Doctype", "Title", "Body", "The heading"],
-      answer: 0
+      answers: ["Doctype", "Title", "Body", "The heading"],
+      correct: 0
     },
     {
       question: "Which of the following is described as a collection of images put in a single image?",
-      options: ["Float", "Align", "Sprite", "Image"],
-      answer: 2
+      answers: ["Float", "Align", "Sprite", "Image"],
+      correct: 2
     },
     {
       question: "A collection of data containing both properties and methods is called...",
-      options: ["Object", "Selector", "Class", "Tag"],
-      answer: 0
+      answers: ["Object", "Selector", "Class", "Tag"],
+      correct: 0
     },
     {
       question: "In JavaScript, 'this' refers to the object that ____ the object.",
-      options: ["Direct", "Receives", "Depends", "Owns"],
-      answer: 3
+      answers: ["Direct", "Receives", "Depends", "Owns"],
+      correct: 3
     },
     {
       question: "How can you clear a floated element?",
-      options: ["Del tag", "Press the delete key", "Clear:both", "Strike tag"],
-      answer: 2
+      answers: ["Del tag", "Press the delete key", "Clear:both", "Strike tag"],
+      correct: 2
     },
   ];
